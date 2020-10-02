68b0bd76466e40f1e561bbdab21ca6e4a93d33f1
vijay <vijayakumar@ideas2it.com>
2020-10-02 23:26:53 +0530

    Feat: Adding new library module and included in project.

--- app/src/main/java/vijay/android/projectpersonview/MainActivity.kt
+++ app/src/main/java/vijay/android/projectpersonview/MainActivity.kt
@@ -2,10 +2,16 @@ package vijay.android.projectpersonview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import vijay.android.personview.ToastViewer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvHelloWorld.setOnClickListener {
+            ToastViewer.showSimpleToast(this, "Library Sync Success")
+        }
     }
 }
---
+++ personview/src/main/java/vijay/android/personview/ToastViewer.kt
@@ -0,0 +1,12 @@
+package vijay.android.personview
+
+import android.content.Context
+import android.widget.Toast
+
+public class ToastViewer {
+    companion object {
+        fun showSimpleToast(context: Context, message: String) {
+            Toast.makeText(context,message,Toast.LENGTH_LONG).show()
+        }
+    }
+}
